88569ce0e843e5028d88274a727aec37e77855a7
Jorge Monasterio <jorge.monasterio@unosquare.com>
2023-05-11 15:38:28 -0400

    performance optimization with react query, using the fetch method that we created before

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,4 +1,7 @@
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import fetchPet from './fetchPet';
+
 
 /* how useParams() works? 
 - basically because of the context, and the BrowserRouter that is making this thing called context available to components underneath it,
@@ -8,9 +11,36 @@ in other words is coming from the browser component
 
 const Details = () => {
     const { id } = useParams();
+    //here we give a it a key (id) of what we're requesting
+    //(["details", id], fetchPet) 
+    //the id is from the animal that we want to fetch
+    //the details could be any arbitrary caching key, is like a key from where the data will be store in the cache
+    //the fetchPet method its like we said ok if you dont have that in your cache run this method
+    //for example if you dont hahve "details": 5 in your cache run fetchPet and get it
+    const results = useQuery(["details", id], fetchPet); 
+
+    if(results.isLoading) { //inmediatly the user will see something loading
+        return (
+            <div className="loading-pane">
+                <h2 className="loader">🌀</h2>
+            </div>
+        );
+    }
+
+    //and then as soon as fetchPet completes, its going to go and re render with the correct info
+    const pet = results.data.pets[0]; //thats how it comes back from the api
+
     return(
-        <h2>{id}</h2>
-    )
-}
+        <div className="details">
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>{pet.animal} - {pet.breed} - {pet.city} - {pet.state}
+                    <button>Adopt {pet.name}</button>
+                    <p>{pet.description}</p>
+                </h2>
+            </div>
+        </div>
+    );
+};
 
 export default Details;
